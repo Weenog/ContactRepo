06badb3a9d89a2597818cb3e7d764aa9b21730cc
Thomas Vanhuffel <thomasvanhuffel@hotmail.com>
2020-06-09 14:23:51 +0200

    Test2

---
+++ ContactListApp/Models/ContactModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactListApp.Models
+{
+    public class Contact
+    {
+        string LastName;
+        string FirstName;
+        string Email;
+        int    TelNr;
+        string Adress;
+        string Description;
+    }
+}
